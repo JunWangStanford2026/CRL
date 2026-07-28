dec9391bbb7ce76b066ea08c141b2c8f328e0dfd
EC2 Default User <ec2-user@ip-172-31-33-73.ec2.internal>
2026-07-28 16:54:22 +0000

    new results

--- run_minilang.sh
+++ run_minilang.sh
@@ -1,3 +1,3 @@
 cd ~/CRL
 conda activate CRL
-nohup python minilang/run_experiments.py --effective-horizon 56000 --block-size 2 --num-blocks 2 --num-models 32 --evals-per-model 32 --results-dir minilang/results &
+nohup python minilang/run_experiments.py --effective-horizon 112000 --block-size 2 --num-blocks 2 --num-models 32 --evals-per-model 32 --results-dir minilang/results &
